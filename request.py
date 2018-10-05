8c575c16d74cf18acb2a92d5c47130408c97d60e
John Lawrence <jalawrence6023@gmail.com>
2018-10-05 11:54:25 -0400

    standalone mode

--- request.py
+++ request.py
@@ -6,7 +6,7 @@ from lumavate_signer import Signer
 from app import app, db
 from base64 import b64decode
 from lumavate_request import ApiRequest
-from lumavate_exceptions import ApiException
+from lumavate_exceptions import ApiException, AuthorizationException
 from enum import Enum
 import requests
 import json
@@ -30,46 +30,27 @@ def __authenticate(storage, key, security_type):
     restrict_domain = False
 
   valid_header = False
-  if key in storage:
-    try:
-      jwt = storage.get(key)
-      header, payload, signature = jwt.replace('Bearer ', '').split('.')
-      token_data = json.loads(b64decode(payload + '==').decode('utf-8'))
-      g.pwa_jwt = jwt.replace('Bearer ', '')
-      g.token_data = token_data
-      g.org_id = token_data.get('orgId')
 
-      if restrict_domain == True:
-        if re.match('(' + str(g.token_data.get('code')) + '|' + str(g.token_data.get('namespace')) + ')[\.\_\-]', request.host) is None:
-          raise Exception('Not Authorized')
+  jwt = get_lumavate_request().get_token(storage, key)
+  header, payload, signature = jwt.replace('Bearer ', '').split('.')
+  token_data = json.loads(b64decode(payload + '==').decode('utf-8'))
+  g.pwa_jwt = jwt.replace('Bearer ', '')
+  g.token_data = token_data
+  g.org_id = token_data.get('orgId')
 
-      try:
-        service_data = LumavateRequest().get_service_data(request.headers.get('Lumavate-sut'))
-        g.service_data = service_data['serviceData']
-        g.session = service_data['session']
-      except:
-        if security_type != SecurityType.system_origin:
-          raise Exception('Not Authorized')
+  if restrict_domain == True:
+    if re.match('(' + str(g.token_data.get('code')) + '|' + str(g.token_data.get('namespace')) + ')[\.\_\-]', request.host) is None:
+      raise AuthorizationException('Domain mismatch')
 
-      if check_sut == True and service_data and service_data.get('sutValid', True) == False:
-        raise Exception('Not Authorized')
+  service_data = get_lumavate_request().get_service_data(request.headers.get('Lumavate-sut'))
+  g.service_data = service_data['serviceData']
+  g.session = service_data['session']
 
+  if check_sut == True and service_data and service_data.get('sutValid', True) == False:
+    raise Exception('Not Authorized')
 
-      g.auth_status = {
-        'status': 'inactive',
-        'roles': [],
-        'user': 'anonymous'
-      }
-      try:
-        if g.token_data.get('authUrl') and str(g.token_data.get('authUrl')).split('/')[-1] != os.environ.get('SERVICE_NAME'):
-          g.auth_status = LumavateRequest().get('https://' + request.host + g.token_data.get('authUrl') + '/status')
-      except:
-        pass
-
-    except Exception as e:
-      raise Exception('Not Authorized')
-
-    valid_header = True
+  g.auth_status = get_lumavate_request().get_auth_status()
+  valid_header = True
 
   if valid_header == False:
     raise Exception('Not Authorized')
@@ -80,16 +61,15 @@ def api_response(security_type, required_roles=[]):
     def wrapped(*args, **kwargs):
       try:
         __authenticate(request.headers, 'Authorization', security_type)
-      except:
-        return make_response('Not Authenticated', 401)
+      except ApiException as e:
+        return jsonify(e.to_dict()), e.status_code
 
       try:
         r = f(*args, **kwargs)
         db.session.commit()
       except ApiException as e:
+        print(e, flush=True)
         return jsonify(e.to_dict()), e.status_code
-      except:
-        raise
 
       if r is None:
         abort(404)
@@ -122,6 +102,18 @@ def browser_response(f):
     return r
   return wrapped
 
+alt_lumavate_request_factory = None
+
+def set_lumavate_request_factory(func):
+  global alt_lumavate_request_factory
+  alt_lumavate_request_factory = func
+
+def get_lumavate_request():
+  if alt_lumavate_request_factory is None:
+    return LumavateRequest()
+  else:
+    return alt_lumavate_request_factory()
+
 class LumavateRequest(ApiRequest):
   def sign_url(self, method, path, payload, headers):
     signer = Signer(os.environ.get('PUBLIC_KEY'), os.environ.get('PRIVATE_KEY'))
@@ -134,8 +126,45 @@ class LumavateRequest(ApiRequest):
     return 'https://' + request.host
     #return os.environ.get('BASE_URL')
 
+  def get_token(self, storage, key):
+    return storage.get(key)
+
   def get_service_data(self, sut):
     sut_qs = ''
     if sut is not None:
       sut_qs = '?sut=' + sut
-    return self.get('/pwa/v1/service-instances/' + os.environ.get('SERVICE_NAME') + sut_qs)
+    d = self.get('/pwa/v1/service-instances/' + os.environ.get('SERVICE_NAME') + sut_qs)
+    #with open('/app/service-data.json', 'w') as outfile:
+    #  json.dump(d, outfile, sort_keys=True, indent=4)
+    return d
+
+  def get_auth_status(self):
+    auth_status = {
+      'status': 'inactive',
+      'roles': [],
+      'user': 'anonymous'
+    }
+    try:
+      if g.token_data.get('authUrl') and str(g.token_data.get('authUrl')).strip('/').split('/')[-1] != os.environ.get('SERVICE_NAME'):
+        auth_status = LumavateRequest().get('https://' + request.host + g.token_data.get('authUrl') + 'status')
+    except Exception as e:
+      pass
+
+    #with open('/app/auth-data.json', 'w') as outfile:
+    #  json.dump(auth_status, outfile, sort_keys=True, indent=4)
+    return auth_status
+
+class LumavateMockRequest(LumavateRequest):
+  def __init__(self, mock_service, mock_auth, mock_token):
+    self._mock_service = mock_service
+    self._mock_auth = mock_auth
+    self._mock_token = mock_token
+
+  def get_service_data(self, sut):
+    return self._mock_service
+
+  def get_auth_status(self):
+    return self._mock_auth
+
+  def get_token(self, storage, key):
+    return self._mock_token
