d0224e73e99eb7fc58b952e932e4c56cfecf598a
John Lawrence <john@Johns-MacBook-Pro-2.local>
2018-11-07 10:15:49 -0500

    add basic support for any route match

--- route_decorator.py
+++ route_decorator.py
@@ -96,6 +96,7 @@ def lumavate_route(path, methods, request_type, security_types, required_roles=[
     regex_path = path
 
     regex_path = re.sub('<string[^>]*>','[^/]*', regex_path)
+    regex_path = re.sub('<any[^>]*>','[^/]*', regex_path)
     regex_path = re.sub('<int[^>]*>','[0-9-]*', regex_path)
 
     all_routes.append({
