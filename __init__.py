902c168ddc1692cfcd3c1578e7c198788a4b86cb
John Lawrence <john@Johns-MacBook-Pro-2.local>
2018-11-07 09:50:17 -0500

    refactor routes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 from .paging import Paging
 from .rest import RestBehavior, icon_blueprint, make_id
-from .request import api_response, browser_response, LumavateRequest, LumavateMockRequest, SecurityType, get_lumavate_request, set_lumavate_request_factory
+from .request import api_response, browser_response, LumavateRequest, LumavateMockRequest, get_lumavate_request, set_lumavate_request_factory
 from .dev_mock import DevMock
+from .route_decorator import lumavate_route, lumavate_blueprint, all_routes
+from .security_type import SecurityType
+from .request_type import RequestType
