eea16a121e1ac038bd0c4fe147084451c95586fb
Andrew Jackson <andrew.c.j1995@gmail.com>
2021-04-23 10:49:55 -0400

    AAF-37: Add jsonb string contains filter

--- filter.py
+++ filter.py
@@ -35,7 +35,7 @@ class Filter:
           self.args[arg_key] = str(arg_value)
 
   def apply(self, base_query):
-    ops = ['eq', 'neq', 'gt', 'gte', 'lt', 'lte', 'sw', 'ct','aeq', 'adeq', 'act']
+    ops = ['eq', 'neq', 'gt', 'gte', 'lt', 'lte', 'sw', 'ct','aeq', 'adeq', 'act', 'jsonbstrct']
     for a in self.args:
       if a != 'sort' and a not in self.ignore_fields:
         parts = self.args[a].split(":", 1)
@@ -106,6 +106,8 @@ class Filter:
         return and_(column.op('@>')(value),column.op('<@')(value))
       elif op == 'act':
         return column.contains(value)
+      elif op == 'jsonbstrct':
+        return column.op('?')(value)
 
 
 class ColumnResolver:
