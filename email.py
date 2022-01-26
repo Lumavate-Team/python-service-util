3a4333a01d2122acdf6ead851a72858d9b832c31
abernathyc <72395529+abernathyc@users.noreply.github.com>
2022-01-26 09:02:55 -0500

    PLAT3-1613; Removing luthor email dependency (#51)
    * PLAT3-1613; Adding base point for sending email
    
    * PLAT3-1613; Changing dict to to_json
    
    * PLAT3-1613 Judging sync based on schemaLastModifiedDate
    
    * PLAT3-1631 Changing email to be not dependent on luthor

--- email.py
+++ email.py
@@ -1,5 +1,6 @@
 from .request import get_lumavate_request
 from flask import g, request
+import boto3
 import os
 import rollbar
 
@@ -12,19 +13,52 @@ class Email():
     
     if not isinstance(recipients, list):
       recipients = recipients.split(',')
+      
+    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 
-    email_data = {
-      'toAddresses': recipients,
-      'subject': subject,
-      'htmlContent': body
-    }
+    if not aws_access_key_id:
+      rollbar.report_message('AWS_ACCESS_KEY_ID is not set attempting when send an email')
+      return
 
-    try:
-      get_lumavate_request().post(notification_url, payload=email_data)
-    except Exception as e:
-      rollbar.report_message(f'Error sending email: {e}')
+    if aws_access_key_id == 'dev-key':
+      print('Subject: ', subject, flush=True)
+      print('To: ', recipients, flush=True)
+      print('Message: ', body, flush=True)
+    else:
+      try:
+        region_name = os.environ.get('AWS_REGION')
+        aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    return
+        if not region_name or not aws_secret_access_key:
+          rollbar.report_message('Environment is not configured for attempting to send an email')
+          return
+
+        client = boto3.client('ses',
+          region_name=region_name,
+          aws_access_key_id=aws_access_key_id,
+          aws_secret_access_key=aws_secret_access_key)
+
+        client.send_email(
+          Destination={
+            'ToAddresses': recipients
+          },
+          Message={
+            'Body': {
+              'Html': {
+                'Data': body,
+              }
+            },
+            'Subject': {
+                'Data': subject,
+            },
+          },
+          Source='noreply@' + request.host.replace('_', '-')
+        )
+
+      except Exception as e:
+        rollbar.report_message(f'Error sending email: {e}')
+
+    return { 'status': 'sent' }
 
     
 
