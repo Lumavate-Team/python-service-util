82e4321a39c3fae9f602dbf9704588b871fad355
Kris Hill <kristopher.hill@gmail.com>
2022-03-17 13:29:24 -0700

    SUR-15 removed print statements

--- base_asset/models/data_model.py
+++ base_asset/models/data_model.py
@@ -83,8 +83,6 @@ class DataBaseModel(BaseModel):
       data.replace("'", '"')
       data = json.loads(data)
 
-
-    print(dir(data),flush=True)
     schema_columns = self.get_column_definitions(self.asset_id)
     column_dict = {column_def.get('columnName'): DataColumn.from_json(column_def) for column_def in schema_columns}
     time_patterns = self.get_compiled_time_patterns()
@@ -92,7 +90,6 @@ class DataBaseModel(BaseModel):
 
     for data_key, value in data.items():
       column = column_dict.get(data_key.lower())
-      print(column, flush=True)
       if column is None:
         # not all submitted values are tied to the columns, some are asset related, just skip them
         continue
