0e34dc1a2fe5e7af7b2d2a69f20708518bc47f86
abernathyc <72395529+abernathyc@users.noreply.github.com>
2023-07-26 16:28:58 -0400

    PLAT3-3639; add asset id to object to allow for use in override (#90)

--- base_asset/data_filter.py
+++ base_asset/data_filter.py
@@ -5,6 +5,7 @@ from ..filter import Filter
 class DataRestFilter(Filter):
   def __init__(self, model, asset_id, args=None, ignore_fields=None):
     self.model = model
+    self.asset_id = asset_id
     schema_columns = self.model.get_column_definitions(asset_id)
     self.columns = {column_def.get('columnName'): DataColumn.from_json(column_def) for column_def in schema_columns}
     super().__init__(args, ignore_fields)
