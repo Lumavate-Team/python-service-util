946be300343906a57e6f8c0df4fa9689fbdcdd72
Eric Seidel <e.seidel@lumavate.com>
2023-08-15 11:05:26 -0400

    PLAT3-3763 added tag filtering to file_asset_rest

--- base_asset/file_filter.py
+++ base_asset/file_filter.py
@@ -1,11 +1,12 @@
 from sqlalchemy.sql.expression import cast
 import sqlalchemy.dialects.postgresql
-from sqlalchemy import or_, String, and_, func
+from sqlalchemy import or_, String, and_, func, select
 from flask import request, g
 from dateutil.parser import parse
 from lumavate_exceptions import ValidationException
 from sqlalchemy.inspection import inspect
 from ..filter import Filter
+from .models import AssetCategoryModel, CategoryModel
 import re
 
 class FileFilter(Filter):
@@ -21,5 +22,11 @@ class FileFilter(Filter):
         cast(column, String).ilike('%' + value.strip() + '%')
       ]
       return base_query.filter(or_(*[c for c in clauses]))
+    if op == 'eq' and column_name == 'tags':
+      values = value.split('||')
+
+      return base_query.filter(\
+        self.get_column(base_query, 'id').\
+          in_(AssetCategoryModel.get_all_by_type_and_ids('tag', values).with_entities(AssetCategoryModel.asset_id)))
     else:
       return super().apply_column(base_query, column_name, op, value)
