ae95b705a2bcc6ed7a860ded780287a5d83a51aa
Chris Abernathy <c.abernathy@lumavate.com>
2023-08-31 11:02:54 -0400

    PLAT3-3950; batch validate tag names

--- base_asset/behavior/tag_rest.py
+++ base_asset/behavior/tag_rest.py
@@ -22,11 +22,30 @@ class TagRestBehavior(CategoryRestBehavior):
         .filter(func.lower(self._model_class.name) == func.lower(property_data.get('name'))).first()
 
     if existing_tags is not None and (record_id is None or existing_tags.id != record_id):
-      raise ValidationException('Name is already taken.', 'name')
+      name = property_data.get('name')
+      raise ValidationException(f'Tag name: {name} is already in use.', 'name')
+
+  def batch_validate_names(self, data):
+    new_data = [tag for tag in data if tag.get('operation', '') and tag.get('operation') != 'delete']
+    names = [tag['name'].lower() for tag in new_data]
+    existing_tags = self._model_class.get_all_by_type('tag') \
+        .filter(func.lower(self._model_class.name).in_(names))
+
+    for tag in existing_tags:
+      found = False
+      for current_tag in new_data:
+        if tag.id == current_tag.get('id', ''):
+          found = True
+          break
+      if not found:
+        name = tag.name
+        raise ValidationException(f'Tag name: {name} is already in use.', 'name')
 
   def batch_tags(self):
     data = self.get_data().get('data', [])
     response = []
+
+    self.batch_validate_names(data)
 
     for tag in data:
       operation = tag.get('operation', '')
@@ -39,10 +58,8 @@ class TagRestBehavior(CategoryRestBehavior):
         raise ValidationException("Invalid tag name", api_field='name')
 
       if operation == 'add':
-        self.validate_tag_name(tag)
         response.append(handler.post())
       if operation == 'modify':
-        self.validate_tag_name(tag, tag['id'])
         response.append(handler.put(tag['id']))
       if operation == 'delete':
         response.append(handler.delete(tag['id']))
