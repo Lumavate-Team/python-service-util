a80f7085997fca8c991795ec2db23f823c08c729
Eugene Quilter <e.quilter@lumavate.com>
2024-02-20 11:32:59 -0500

    PLAT3-4272; Fixed event imports.

--- base_asset/models/__init__.py
+++ base_asset/models/__init__.py
@@ -27,6 +27,6 @@ from .abstract_asset_type_model import AbstractAssetTypeModel
 from .abstract_asset_type_access_model import AbstractAssetTypeAccessModel
 
 from .event.event_type import EventTypeModel
-from .event.event import EventModel
+from .event.event_model import EventModel
 from .event.event_type_access import EventTypeAccessModel
 from .event.events_related_products import EventsRelatedProductsModel
