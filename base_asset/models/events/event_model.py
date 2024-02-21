e5cb38fa2e221de75a3a7f634222d6da0513d3c1
Eugene Quilter <e.quilter@lumavate.com>
2024-02-21 10:09:00 -0500

    PLAT3-4272; Fixed event imports.

--- base_asset/models/events/event_model.py
+++ base_asset/models/events/event_model.py
@@ -11,8 +11,8 @@ from ....db import BaseModel, Column
 from ....aws import FileBehavior
 from ...column import DataColumn
 from ...models.asset_model import AssetBaseModel
-from events.events_related_products import EventsRelatedProductsModel
-from events.event_model import EventTypeModel
+# from events.events_related_products import EventsRelatedProductsModel
+# from events.event_model import EventTypeModel
 from json import loads
 from dateutil.parser import *
 from dateutil.tz import *
@@ -181,57 +181,60 @@ class EventModel(BaseModel):
 
   @classmethod
   def get_all_overview(cls, asset_id, args=None):
-    asset = EventTypeModel
-    column_cte = select([
-        asset.id,
-        cast(asset.data.op('->')('headlineField').op('->>')('columnName'), Text).label('headlineField'), 
-        case([(asset.data.op('->')('headlineField').op('->>')('id') == None, True)], else_=False).label('isHeadlineBase'), 
-        coalesce(cast(asset.data.op('->')('subheadlineField').op('->>')('columnName'), Text), 'sku').label('subheadlineField'),
-        case([(asset.data.op('->')('subheadlineField').op('->>')('id') == None, True)], else_=False).label('isSubheadlineBase')])\
-      .select_from(asset)\
-      .where(and_(asset.org_id == g.org_id, asset.id == asset_id)) \
-      .cte('asset_columns')
+    pass
+    # asset = EventTypeModel
+    # column_cte = select([
+    #     asset.id,
+    #     cast(asset.data.op('->')('headlineField').op('->>')('columnName'), Text).label('headlineField'), 
+    #     case([(asset.data.op('->')('headlineField').op('->>')('id') == None, True)], else_=False).label('isHeadlineBase'), 
+    #     coalesce(cast(asset.data.op('->')('subheadlineField').op('->>')('columnName'), Text), 'sku').label('subheadlineField'),
+    #     case([(asset.data.op('->')('subheadlineField').op('->>')('id') == None, True)], else_=False).label('isSubheadlineBase')])\
+    #   .select_from(asset)\
+    #   .where(and_(asset.org_id == g.org_id, asset.id == asset_id)) \
+    #   .cte('asset_columns')
 
-    return db.session.query(
-        cast(cls.submitted_data.op('->>')('eventName'), Text).label('event_name'),
-        case([
-          (column_cte.c.isHeadlineBase == True, 
-            coalesce(cast(cls.submitted_data.op('->>')(column_cte.c.headlineField), Text),''))],
-          else_ = 
-            coalesce(cast(cls.submitted_data.op('->')('columns').op('->>')(column_cte.c.headlineField),Text),''))\
-        .label('headline_field'),
-        case([
-          (column_cte.c.subheadlineField == None, 
-            None),
-          (column_cte.c.isSubheadlineBase == True,
-            coalesce(cast(cls.submitted_data.op('->>')(column_cte.c.subheadlineField), Text), None))],
-          else_ =
-            coalesce(cast(cls.submitted_data.op('->')('columns').op('->>')(column_cte.c.subheadlineField), Text), ''))\
-        .label('subheadline_field'),
-        cls.public_id,
-        cls.event_type_id
-        )\
-        .select_from(cls)\
-        .join(column_cte, cls.org_id == g.org_id)
+    # return db.session.query(
+    #     cast(cls.submitted_data.op('->>')('eventName'), Text).label('event_name'),
+    #     case([
+    #       (column_cte.c.isHeadlineBase == True, 
+    #         coalesce(cast(cls.submitted_data.op('->>')(column_cte.c.headlineField), Text),''))],
+    #       else_ = 
+    #         coalesce(cast(cls.submitted_data.op('->')('columns').op('->>')(column_cte.c.headlineField),Text),''))\
+    #     .label('headline_field'),
+    #     case([
+    #       (column_cte.c.subheadlineField == None, 
+    #         None),
+    #       (column_cte.c.isSubheadlineBase == True,
+    #         coalesce(cast(cls.submitted_data.op('->>')(column_cte.c.subheadlineField), Text), None))],
+    #       else_ =
+    #         coalesce(cast(cls.submitted_data.op('->')('columns').op('->>')(column_cte.c.subheadlineField), Text), ''))\
+    #     .label('subheadline_field'),
+    #     cls.public_id,
+    #     cls.event_type_id
+    #     )\
+    #     .select_from(cls)\
+    #     .join(column_cte, cls.org_id == g.org_id)
 
   @classmethod
   def get_column_definitions(cls, asset_id):
-    return EventTypeModel.get_column_definitions(asset_id)
+    pass
+    # return EventTypeModel.get_column_definitions(asset_id)
 
   @classmethod
   def get_related_product_ids(cls, data_id):
-    d = cls
-    r = EventsRelatedProductsModel
+    pass
+    # d = cls
+    # r = EventsRelatedProductsModel
 
-    event_query = select([r.event_id])\
-      .select_from(r)\
-      .where(and_(r.product_id == data_id, r.org_id == g.org_id))
+    # event_query = select([r.event_id])\
+    #   .select_from(r)\
+    #   .where(and_(r.product_id == data_id, r.org_id == g.org_id))
 
-    product_query = select([r.product_id])\
-      .select_from(r)\
-      .where(and_(r.event_id == data_id, r.org_id == g.org_id))
+    # product_query = select([r.product_id])\
+    #   .select_from(r)\
+    #   .where(and_(r.event_id == data_id, r.org_id == g.org_id))
 
-    return union(event_query, product_query)
+    # return union(event_query, product_query)
 
   #TODO: consolidate with table builder into service util  DataBaseModel
   @classmethod
